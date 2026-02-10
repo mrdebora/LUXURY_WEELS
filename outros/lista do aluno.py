01a4e00e64368afd2837a9938577b7de312bcea9
mrsDebora <deboravaz789@gmail.com>
2026-02-10 16:14:49 +0000

    Delete outros directory

--- outros/lista do aluno.py
+++
@@ -1,8 +0,0 @@
-#recebe_o_nome
-nome = str(input("digite seu nome como estudante»»"))
-#recebe_a_idade
-idade = int(input("digite sua idade»»"))
-#recebe_a_nota
-nota = float(input("digite sua nota»»"))
-#apresenta no console tudo adicionado acima
-print("Estudante:",nome,"\nidade:",idade,"\nnota:",nota)
